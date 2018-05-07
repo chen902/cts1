b8572a93126d0ad072560cb34b1ad46e13c35291
chen902 <chen902@gmail.com>
2018-05-07 18:56:25 +0200

    eliminated canonical switch statements added two more states to detect errors in: .324 12.5e+
    added new test cases
    improved output to console

--- FloatDFA/src/floatdfa/FloatDFA.java
+++ FloatDFA/src/floatdfa/FloatDFA.java
@@ -20,31 +20,33 @@ public class FloatDFA {
     public static void main(String[] args) {
         String word = "12.4325e+66";
         if (FloatDFA.accept(word)) 
-            System.out.println("Accepted!");
-
-        word = "12.4325ef66";
-        if (FloatDFA.accept(word)) 
-            System.out.println("Accepted!");
-
-        word = "12.4325e+f66";
-        if (FloatDFA.accept(word)) 
-            System.out.println("Accepted!");
-        
-        word = ".432";
-        if (FloatDFA.accept(word)) 
-            System.out.println("Accepted!");
-
-        word = "12.e";
-        if (FloatDFA.accept(word)) 
-            System.out.println("Accepted!");
-
-        word = "12.5e+";
-        if (FloatDFA.accept(word)) 
-            System.out.println("Accepted!");
+            System.out.println(word + " Accepted!");
 
         word = "12.432";
         if (FloatDFA.accept(word)) 
-            System.out.println("Accepted!");
+            System.out.println(word + " Accepted!");
+
+        word = "12.4325ef66";
+        if (FloatDFA.accept(word)) 
+            System.out.println(word + " Accepted!");
+
+        word = "12.4325e+f66";
+        if (FloatDFA.accept(word)) 
+            System.out.println(word + " Accepted!");
+        
+        word = ".432";
+        if (FloatDFA.accept(word)) 
+            System.out.println(word + " Accepted!");
+
+        word = "12.e";
+        if (FloatDFA.accept(word)) 
+            System.out.println(word + " Accepted!");
+
+        word = "12.5e+";
+        if (FloatDFA.accept(word)) 
+            System.out.println(word + " Accepted!");
+
+
     }
 
     public static boolean accept(String word) {
@@ -56,53 +58,71 @@ public class FloatDFA {
             position++;
             switch (state) {
                 case 0:
-                    // function [0-9] = state 0
-                    // function '.' = state 1
-                    switch (ch) {
-                        case '.':           // function '.'
-                            state = 1;
-                            break;
-                        default:
-                            if (!Character.isDigit(ch)) {     // function '[0-9]'
-                                System.out.println("Error! For word: "+ word +posIndicator(position)+"Expected '.' or a digit is position: " + position);
-                                return false;
-                            }
+                    // function [0-9] -> state 1
+                    if (Character.isDigit(ch)) {     
+                        state = 1;
+                    } 
+                    else{
+                        System.out.println("Error! For word: "+ word +posIndicator(position)+"Expected a digit is position: " + position);
+                        return false;                        
                     }
                     break;
                 case 1:
-                    // function [0-9] = state 2
-                    if (Character.isDigit(ch)) {      // function '[0-9]' 
+                    // function [0-9] -> state 1
+                    // function '.' -> state 2
+                    if(ch == '.'){
                         state = 2;
-                    } else {
-                        System.out.println("Error! For word: "+ word + posIndicator(position)+ "Expected a digit in position: " + position);
+                    } else if(!Character.isDigit(ch)){
+                        System.out.println("Error! For word: "+ word + posIndicator(position)+ "Expected a digit or '.' in position: " + position);
                         return false;
                     }
                     break;
                 case 2:
-                    // function [0-9] = state 2
-                    // function e,E = state 3
+                    // function [0-9] -> state 3
+                    if(Character.isDigit(ch)){
+                       state = 3; 
+                    }
+                    else {
+                        System.out.println("Error! For word: "+ word +posIndicator(position)+"Expected a digit is position: " + position);
+                        return false;
+                    }
+                    break;
+                case 3:
+                    // function [0-9] -> state 3
+                    // function e,E -> state 4
                     if (Character.toLowerCase(ch) == 'e') {
-                        state = 3;
+                        state = 4;
                     } else if (!Character.isDigit(ch)) {
                         System.out.println("Error! For word: "+ word + posIndicator(position)+ "Expected 'e/E' or a digit in position: " + position);
                         return false;
                     }
                     break;
-                case 3:
-                    // function +,-,[0-9] = state 4
-                    if(ch == '+' || ch == '-' || Character.isDigit(ch))
-                        state = 4;
-                    else{
+                case 4:
+                    // function +,- -> state 5
+                    // function [0-9] -> state 6
+                    if(ch == '+' || ch == '-')
+                        state = 5;
+                    else if(Character.isDigit(ch)){
+                        state = 6;
+                    }else{
                         System.out.println("Error! For word: "+ word + posIndicator(position)+"Expected '+/-' or a digit in position: " + position);
                         return false;                    
                     }
                     break;
-                case 4:
-                    // function [0-9] = state 4
-                    // accept state 
-                    if (!Character.isDigit(ch)) {
+                case 5:
+                    // function [0-9] -> state 6
+                    if (Character.isDigit(ch)) {
+                        state = 6;
+                    } else{
                         System.out.println("Error! For word: "+ word + posIndicator(position)+"Expected a digit in position: " + position);
-                        return false;
+                        return false;                        
+                    }
+                    break;
+                case 6:
+                    // function [0-9] -> state 6
+                    // accept state
+                    if(!Character.isDigit(ch)){
+                        System.out.println("Error! For word: "+ word + posIndicator(position)+"Expected a digit in position: " + position);
                     }
                     break;
             }
@@ -122,6 +142,6 @@ public class FloatDFA {
     
     // accept states are 2 and 4
     private static boolean isAcceptState(int state) {
-        return (state == 2) || (state == 4);
+        return (state == 3) || (state == 6);
     }
 }
